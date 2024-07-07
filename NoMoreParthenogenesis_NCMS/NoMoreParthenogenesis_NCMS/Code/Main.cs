e736d562282e1f8c8385f24d5ef86e230c4f84e1
Jean-Antoine d'Anglerais <zzzzzzzorro@mail.ru>
2024-07-07 20:58:25 +0300

    Added NML/NCMS version

---
+++ NoMoreParthenogenesis_NCMS/NoMoreParthenogenesis_NCMS/Code/Main.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using NCMS;
+using System.Reflection;
+using UnityEngine;
+using ai.behaviours;
+
+namespace NoMoreParthenogenesis_NCMS
+{
+    [ModEntry]
+    class Main : MonoBehaviour
+    {
+        public static Harmony harmony = new Harmony(MethodBase.GetCurrentMethod().DeclaringType.Namespace);
+
+        void Awake()
+        {
+            Localizer.SetLocalization("ru", "creature_statistics_gender", "Пол");
+            Localizer.SetLocalization("en", "creature_statistics_gender", "Gender");
+
+            Localizer.SetLocalization("ru", "gender_unknown", "Неизвестно");
+            Localizer.SetLocalization("en", "gender_unknown", "Unknown");
+
+            Localizer.SetLocalization("ru", "gender_male", "Мужской");
+            Localizer.SetLocalization("en", "gender_male", "Male");
+
+            Localizer.SetLocalization("ru", "gender_female", "Женский");
+            Localizer.SetLocalization("en", "gender_female", "Female");
+
+            harmony.Patch(AccessTools.Method(typeof(CityBehProduceUnit), "produceNewCitizen"),
+            transpiler: new HarmonyMethod(AccessTools.Method(typeof(Patches), "produceNewCitizen_Transpiler")));
+
+            harmony.Patch(AccessTools.Method(typeof(WindowCreatureInfo), "OnEnable"),
+            transpiler: new HarmonyMethod(AccessTools.Method(typeof(Patches), "OnEnable_Transpiler")));
+        }
+    }
+}
---
+++ NoMoreParthenogenesis_NCMS/NoMoreParthenogenesis_NCMS/Code/StaticStuff.cs
@@ -0,0 +1,29 @@
+﻿namespace NoMoreParthenogenesis_NCMS
+{
+    public static class StaticStuff
+    {
+        public static bool ifCantProduceNewCitizen(Actor parent1, Actor parent2)
+        {
+            if (parent2 == null)
+            {
+                return true;
+            }
+
+            else if (parent1 == null)
+            {
+                return true;
+            }
+
+            else if (parent1.data.gender == parent2.data.gender)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static void showGender(WindowCreatureInfo instance)
+        {
+            instance.showStat("creature_statistics_gender", LocalizedTextManager.getText("gender_" + instance.actor.data.gender.ToString().ToLower(), null));
+        }
+    }
+}
